8ec2a3c6a7d1d6d7fcaf699f19c7710be0c1e297
Kriti Sondhi <kritisondhi@gmail.com>
2019-09-16 06:40:46 +0000

    comments in database and comment form

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,16 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
+
+  geocoded_by :address
+  after_validation :geocode
+
+
+
     validates :name, presence: true
+    
+    #validates :name, length: { minimum: 3 }
+    #validates :address, :description, presence: true
 end 
+
